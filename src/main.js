57f94e71a0e5ed040570b7054a37891e4923ccd2
Wojciech Urbanski <wojciech.urbanski@senti1.com>
2017-06-26 22:58:25 +0200

    25. Extracting things to separate modules + adding raw options exports for better testability

--- src/main.js
+++ src/main.js
@@ -1,48 +1,19 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router';
 import Vuelidate from 'vuelidate';
 import { sync } from 'vuex-router-sync';
 import store from './store';
+import router from './router';
 import App from './App.vue'
-import ProductsList from './components/ProductstList';
-import ProductDetails from './components/ProductDetails';
-import ProductForm from './components/ProductForm';
+
 import asCurrency from './filters/asCurrency';
+import styleWhenBroken from './directives/styleWhenBroken';
 
 Vue.config.productionTip = false
 
 Vue.use(Vuelidate);
-Vue.use(VueRouter);
 
 Vue.filter("asCurrency", asCurrency);
-
-Vue.directive(
-  "style-when-broken", function (el) {
-    el.classList.remove("broken-image");
-    if (!el.onerror) {
-      el.onerror = () => {
-        el.classList.add("broken-image");
-      };
-    }
-  }
-);
-
-const router = new VueRouter({
-  routes: [
-    {
-      path: '/',
-      component: ProductsList
-    },
-    {
-      path: '/product/:productId',
-      component: ProductDetails,
-    },
-    {
-      path: '/product/:productId/edit',
-      component: ProductForm,
-    }
-  ]
-});
+Vue.directive("style-when-broken", styleWhenBroken);
 
 
 sync(store, router);
